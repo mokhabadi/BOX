b74fdb16f2d10fcb1aeca1531b39b68aec72d2d9
Eugene <mokhabadi@gmail.com>
2026-09-08 20:49:42 +0330

    use switch in Writer to avoid action allocation

--- Writer.cs
+++ Writer.cs
@@ -15,47 +15,45 @@ public class Writer(BinaryWriter binaryWriter) : IWriter
 		if (value != null) WriteValue(value);
 		binaryWriter.Write(';');
 	}
-	
-	private void WriteValue<T>(T? value)
+
+	private void WriteValue<T>(T value)
 	{
 		if (value is Array array) binaryWriter.Write7BitEncodedInt(array.Length);
 
-		Action action = value switch
+		switch (value)
 		{
-			bool x => () => binaryWriter.Write(x),
-			char x => () => binaryWriter.Write(x),
-			byte x => () => binaryWriter.Write(x),
-			sbyte x => () => binaryWriter.Write(x),
-			short x => () => binaryWriter.Write(x),
-			ushort x => () => binaryWriter.Write(x),
-			int x => () => binaryWriter.Write(x),
-			uint x => () => binaryWriter.Write(x),
-			long x => () => binaryWriter.Write(x),
-			ulong x => () => binaryWriter.Write(x),
-			float x => () => binaryWriter.Write(x),
-			double x => () => binaryWriter.Write(x),
-			decimal x => () => binaryWriter.Write(x),
-			string x => () => binaryWriter.Write(x),
-			DateTime x => () => binaryWriter.Write(x.ToBinary()),
-			TimeSpan x => () => binaryWriter.Write(x.Ticks),
-			IBox x => () => WriteObject(x),
-			bool[] x => () => WriteArray(x, binaryWriter.Write),
-			char[] x => () => binaryWriter.Write(x),
-			byte[] x => () => binaryWriter.Write(x),
-			short[] x => () => WriteArray(x, binaryWriter.Write),
-			int[] x => () => WriteArray(x, binaryWriter.Write),
-			long[] x => () => WriteArray(x, binaryWriter.Write),
-			float[] x => () => WriteArray(x, binaryWriter.Write),
-			double[] x => () => WriteArray(x, binaryWriter.Write),
-			decimal[] x => () => WriteArray(x, binaryWriter.Write),
-			string[] x => () => WriteArray(x, binaryWriter.Write),
-			DateTime[] x => () => WriteArray(x, x => binaryWriter.Write(x.ToBinary())),
-			TimeSpan[] x => () => WriteArray(x, x => binaryWriter.Write(x.Ticks)),
-			IBox[] x => () => WriteArray(x, WriteObject),
-			_ => throw new NotSupportedException(typeof(T).Name)
-		};
-
-		action();
+			case bool x: binaryWriter.Write(x); break;
+			case char x: binaryWriter.Write(x); break;
+			case byte x: binaryWriter.Write(x); break;
+			case sbyte x: binaryWriter.Write(x); break;
+			case short x: binaryWriter.Write(x); break;
+			case ushort x: binaryWriter.Write(x); break;
+			case int x: binaryWriter.Write(x); break;
+			case uint x: binaryWriter.Write(x); break;
+			case long x: binaryWriter.Write(x); break;
+			case ulong x: binaryWriter.Write(x); break;
+			case float x: binaryWriter.Write(x); break;
+			case double x: binaryWriter.Write(x); break;
+			case decimal x: binaryWriter.Write(x); break;
+			case string x: binaryWriter.Write(x); break;
+			case DateTime x: binaryWriter.Write(x.ToBinary()); break;
+			case TimeSpan x: binaryWriter.Write(x.Ticks); break;
+			case IBox x: WriteObject(x); break;
+			case bool[] x: WriteArray(x, binaryWriter.Write); break;
+			case char[] x: binaryWriter.Write(x); break;
+			case byte[] x: binaryWriter.Write(x); break;
+			case short[] x: WriteArray(x, binaryWriter.Write); break;
+			case int[] x: WriteArray(x, binaryWriter.Write); break;
+			case long[] x: WriteArray(x, binaryWriter.Write); break;
+			case float[] x: WriteArray(x, binaryWriter.Write); break;
+			case double[] x: WriteArray(x, binaryWriter.Write); break;
+			case decimal[] x: WriteArray(x, binaryWriter.Write); break;
+			case string[] x: WriteArray(x, binaryWriter.Write); break;
+			case DateTime[] x: WriteArray(x, x => binaryWriter.Write(x.ToBinary())); break;
+			case TimeSpan[] x: WriteArray(x, x => binaryWriter.Write(x.Ticks)); break;
+			case IBox[] x: WriteArray(x, WriteObject); break;
+			default: throw new NotSupportedException(value!.ToString());
+		}
 	}
 
 	private void WriteObject(IBox box)
