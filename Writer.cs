694d50a4d68cb5482afb80167b1db2bdc461a297
Eugene <mokhabadi@gmail.com>
2026-09-09 00:52:51 +0330

    use if/else instead of switch/case in Writer

--- Writer.cs
+++ Writer.cs
@@ -19,41 +19,37 @@ public class Writer(BinaryWriter binaryWriter) : IWriter
 	private void WriteValue<T>(T value)
 	{
 		if (value is Array array) binaryWriter.Write7BitEncodedInt(array.Length);
-
-		switch (value)
-		{
-			case bool x: binaryWriter.Write(x); break;
-			case char x: binaryWriter.Write(x); break;
-			case byte x: binaryWriter.Write(x); break;
-			case sbyte x: binaryWriter.Write(x); break;
-			case short x: binaryWriter.Write(x); break;
-			case ushort x: binaryWriter.Write(x); break;
-			case int x: binaryWriter.Write(x); break;
-			case uint x: binaryWriter.Write(x); break;
-			case long x: binaryWriter.Write(x); break;
-			case ulong x: binaryWriter.Write(x); break;
-			case float x: binaryWriter.Write(x); break;
-			case double x: binaryWriter.Write(x); break;
-			case decimal x: binaryWriter.Write(x); break;
-			case string x: binaryWriter.Write(x); break;
-			case DateTime x: binaryWriter.Write(x.ToBinary()); break;
-			case TimeSpan x: binaryWriter.Write(x.Ticks); break;
-			case IBox x: WriteObject(x); break;
-			case bool[] x: WriteArray(x, binaryWriter.Write); break;
-			case char[] x: binaryWriter.Write(x); break;
-			case byte[] x: binaryWriter.Write(x); break;
-			case short[] x: WriteArray(x, binaryWriter.Write); break;
-			case int[] x: WriteArray(x, binaryWriter.Write); break;
-			case long[] x: WriteArray(x, binaryWriter.Write); break;
-			case float[] x: WriteArray(x, binaryWriter.Write); break;
-			case double[] x: WriteArray(x, binaryWriter.Write); break;
-			case decimal[] x: WriteArray(x, binaryWriter.Write); break;
-			case string[] x: WriteArray(x, binaryWriter.Write); break;
-			case DateTime[] x: WriteArray(x, x => binaryWriter.Write(x.ToBinary())); break;
-			case TimeSpan[] x: WriteArray(x, x => binaryWriter.Write(x.Ticks)); break;
-			case IBox[] x: WriteArray(x, WriteObject); break;
-			default: throw new NotSupportedException(value!.ToString());
-		}
+		if (value is IBox box) WriteObject(box);
+		else if (value is bool @bool) binaryWriter.Write(@bool);
+		else if (value is char @char) binaryWriter.Write(@char);
+		else if (value is byte @byte) binaryWriter.Write(@byte);
+		else if (value is sbyte @sbyte) binaryWriter.Write(@sbyte);
+		else if (value is short @short) binaryWriter.Write(@short);
+		else if (value is ushort @ushort) binaryWriter.Write(@ushort);
+		else if (value is int @int) binaryWriter.Write(@int);
+		else if (value is uint @uint) binaryWriter.Write(@uint);
+		else if (value is long @long) binaryWriter.Write(@long);
+		else if (value is ulong @ulong) binaryWriter.Write(@ulong);
+		else if (value is float @float) binaryWriter.Write(@float);
+		else if (value is double @double) binaryWriter.Write(@double);
+		else if (value is decimal @decimal) binaryWriter.Write(@decimal);
+		else if (value is string @string) binaryWriter.Write(@string);
+		else if (value is DateTime dateTime) binaryWriter.Write(dateTime.ToBinary());
+		else if (value is TimeSpan timeSpan) binaryWriter.Write(timeSpan.Ticks);
+		else if (value is IBox[] boxArray) WriteArray(boxArray, WriteObject);
+		else if (value is bool[] boolArray) WriteArray(boolArray, binaryWriter.Write);
+		else if (value is char[] charArray) binaryWriter.Write(charArray);
+		else if (value is byte[] byteArray) binaryWriter.Write(byteArray);
+		else if (value is short[] shortArray) WriteArray(shortArray, binaryWriter.Write);
+		else if (value is int[] intArray) WriteArray(intArray, binaryWriter.Write);
+		else if (value is long[] longArray) WriteArray(longArray, binaryWriter.Write);
+		else if (value is float[] floatArray) WriteArray(floatArray, binaryWriter.Write);
+		else if (value is double[] doubleArray) WriteArray(doubleArray, binaryWriter.Write);
+		else if (value is decimal[] decimalArray) WriteArray(decimalArray, binaryWriter.Write);
+		else if (value is string[] stringArray) WriteArray(stringArray, binaryWriter.Write);
+		else if (value is DateTime[] dateTimeArray) WriteArray(dateTimeArray, dateTime => binaryWriter.Write(dateTime.ToBinary()));
+		else if (value is TimeSpan[] timeSpanArray) WriteArray(timeSpanArray, timeSpan => binaryWriter.Write(timeSpan.Ticks));
+		else throw new NotSupportedException(value!.ToString());
 	}
 
 	private void WriteObject(IBox box)
